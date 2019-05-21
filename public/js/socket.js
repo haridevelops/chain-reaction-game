e6b219bba2807d44fb853ec097a876ca76c92de6
harisudhan <m.harisudhan95@gmail.com>
2019-05-21 23:55:07 +0530

    multi location and two player boiler plate

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,29 +1,185 @@
 const socket = io();
 
+// variables
+var rows = 5;
+var cols = 5;
+
+var rowIndex = 0;
+var colIndex = 0;
+
+var left = '0';
+var right = '0';
+var above = '0';
+var below = '0';
+
+var currentTurn = 'red'
+var nextTurn = 'blue';
+
+var error = false;
+var opponentTurn = false;
+var cell = $('.cell');
 // Elements
 const $cell = document.querySelectorAll('.cell');
 
+// Parsing Query String
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
 $cell.forEach((cell) => {
     cell.addEventListener('click', (event) => {
+        console.log(event.target)
+        if (opponentTurn) return displayErrorMessage('opponent turn');
         let index = findCoordinates(event);
         if (index.row === undefined && index.column === undefined) {
-            $('#info').show();
-            return setTimeout(() => {
-                $('#info').hide();
-            }, 2000);
+            return displayErrorMessage();
         }
-        socket.emit('cellClickEvent', index, (response, error) => {
+        let userIndex = createUserObject(index);
+        socket.emit('cellClickEvent', userIndex, (response, error) => {
             console.log('callback received from server !!', response);
         })
     });    
 })
 
+socket.emit('newUser', { username, room }, (error) => {
+    if (error) {
+        alert(error);
+        location.href = '/';
+    }
+})
+
+socket.on('cellClickEventResponse', (options) => {
+    console.log('event received', options)
+    if (options.user.username == username) {
+        opponentTurn = true;
+    } else opponentTurn = false;
+
+
+    error = false;
+    insertAtom(options, false);
+	if(error) {
+		$('#message').show();
+	} else {
+		changeTurn();	
+	}
+
+});
+
+const insertAtom = (options, trigger) => {
+    var currentCell = $(`#id${options.index.row}${options.index.column}`);
+	var color = currentCell.attr('data-color');
+	var value = Number(currentCell.attr('data-value'));
+	var capacity = Number(currentCell.attr('data-capacity'));
+
+	if(!trigger) {
+		if(color === currentTurn || color === 'blank') {
+			if(value < capacity) {
+				currentCell.attr({
+					'src' : 'img/'+currentTurn+(value+1)+'.png',
+					'data-color' : currentTurn,
+					'data-value': ''+(value+1)
+				});
+			}
+		} else {
+			error = true;
+		}
+	} else {
+		if(value < capacity) {
+			currentCell.attr({
+				'src' : 'img/'+currentTurn+(value+1)+'.png',
+				'data-color' : currentTurn,
+				'data-value': ''+(value+1)
+			});
+		}
+	}
+		
+
+	if(!error) {
+		triggerChainReaction(options);
+	}
+}
+
+function triggerChainReaction(options){
+	var currentCell = $(`#id${options.index.row}${options.index.column}`);
+	var value = currentCell.attr('data-value');
+	var capacity = currentCell.attr('data-capacity');
+
+	if(capacity === value) {
+		currentCell.attr({
+				'src' : 'img/blank.png',
+				'data-color' : 'blank',
+				'data-value': '0'
+			});
+		calculateNeighbours(currentCell);
+		if($(left).length){
+			insertAtom($(left), true);
+		}
+		if($(right).length){
+			insertAtom($(right), true);
+		}
+		if($(above).length){
+			insertAtom($(above), true);
+		}
+		if($(below).length){
+			insertAtom($(below), true);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const createUserObject = (index) => {
+    return {
+        user: { username, room },
+        index,
+    }
+}
 
 const findCoordinates = (event) => {
     return {
         row: event.target.parentNode.parentNode.rowIndex,
-        column: event.target.parentNode.cellIndex,
-        user: 'harisudhan'
+        column: event.target.parentNode.cellIndex
     }
+}
+
+const displayErrorMessage = (errMsg) => {
+    $('#info').show();
+    return setTimeout(() => {
+        $('#info').hide();
+    }, 2000);
+}
+
+function calculateNeighbours(obj) {
+	var x = Number(obj.attr('data-x'));
+	var y = Number(obj.attr('data-y'));
+	leftId = 'img[data-x='+(x-1)+'][data-y='+(y)+']';
+	rightId = 'img[data-x='+(x+1)+'][data-y='+(y)+']';
+	aboveId = 'img[data-x='+(x)+'][data-y='+(y-1)+']';
+	belowId = 'img[data-x='+(x)+'][data-y='+(y+1)+']';
+	left = $(leftId);
+	right = $(rightId);
+	above = $(aboveId);
+	below = $(belowId);
+}
+
+function changeTurn() {
+	if(currentTurn === 'red') {
+		currentTurn = 'blue';
+		nextTurn = 'red';
+		cell.css('background-color', 'blue');
+	} else if(currentTurn === 'blue') {
+		currentTurn = 'red';
+		nextTurn = 'blue';
+		cell.css('background-color', 'red');
+	} else {
+		console.error('currentTurn variable is neither red nor blue');
+	}
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socket = require('socket.io');
-const CurrentUser = require('./utils/users')
+const { addUser, getUser, getUsersInRoom, removeUser } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -17,11 +17,25 @@ app.use(express.static(dirPath));
 io.on("connection", (socket) => {
     console.log('New Web Socket Connection');
 
-    socket.on('cellClickEvent', (index, callback) => {
-        if (CurrentUser === index.user) 
+    socket.on('newUser', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options });
 
-        CurrentUser = index.user;
-        callback(index);
+        if (error) return callback(error)
+
+        socket.join(user.room);
+        callback();
+    })
+
+
+    socket.on('cellClickEvent', (options, callback) => {
+        console.log(options)
+        // const { error, user } = addUser({ id: socket.id, ...options.user });
+        
+        // socket.join(options.user.room);
+
+        io.to(options.user.room).emit('cellClickEventResponse', options);
+        
+        callback(options);
     })
 });
 
